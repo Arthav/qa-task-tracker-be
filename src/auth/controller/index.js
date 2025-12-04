27ae136c1d5ca6f7d65f5dee571a8a0b5c7d626d
Christian Bonafena <christianbonafena7@gmail.com>
2025-12-04 14:54:50 +0700

    fix import

--- src/auth/controller/index.js
+++ src/auth/controller/index.js
@@ -1,4 +1,4 @@
-const db = require('../../services/db');
+const db = require('../../../services/db');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET, TOKEN_EXPIRES_IN } = require('../../config');
