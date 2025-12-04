db4a59d1b553ba150213dcacdec90c0c83a21733
Christian Bonafena <christianbonafena7@gmail.com>
2025-12-04 15:03:46 +0700

    add export jwt secret config

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const config = {
     port: 5000,
     TOKEN_EXPIRES_IN: process.env.TOKEN_EXPIRES_IN,
+    JWT_SECRET: process.env.JWT_SECRET,
 };
 
 module.exports = config;
